8512334bcb2de7cfe60293e0113a19d8844a7b0a
Jylhis <markus@jylhis.com>
2017-02-19 22:51:44 +0200

    Updated bake.sh & .gitignore

--- tools/bake.sh
+++ tools/bake.sh
@@ -4,6 +4,8 @@ IMPACT_LIBRARY=lib/impact/impact.js
 GAME=lib/game/main.js
 # Output file
 OUTPUT_FILE=game.min.js
+# Get project dir
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )/"
 # Change CWD to Impact's base dir and bake!
-#cd ..
+cd $DIR
 php tools/bake.php $IMPACT_LIBRARY $GAME $OUTPUT_FILE
